408344e5c242e373105d1cf7a6b55a821fb339a9
rahmona-henry <rahmona.henry@icloud.com>
2018-06-24 15:18:20 +0100

    create form and connection to hubspot

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('client'))
+
+///GET Routes///
 app.get('/', (req,res) => res.send("Hello DubSpot"))
 app.listen(3000, () => console.log('App listening on port 3000!'))
